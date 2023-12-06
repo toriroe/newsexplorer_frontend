0210817e3aa8f0f5566320cd7d7db6bb9dcde823
Tori <toriroettger@gmail.com>
2023-12-05 21:55:48 -0600

    markup/styles for saved-news page header

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -5,8 +5,18 @@ const SavedNewsHeader = () => {
   return (
     <header className="saved-news__header">
       <Nav />
-      <div>
-        <h2>Saved News Header</h2>
+      <div className="saved-news__header-container">
+        <h2 className="saved-news__header-title">Saved Articles</h2>
+        <p className="saved-news__header-greeting">
+          Elise, you have 5 saved articles
+        </p>
+        <p className="saved-news__header-keywords">
+          By keywords:
+          <span className="saved-news__header-keywords-list">
+            {" "}
+            Nature, Yellowstone, and 2 others
+          </span>
+        </p>
       </div>
     </header>
   );
