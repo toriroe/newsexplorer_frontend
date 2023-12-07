5591ba8bf05cc14ebb3d17c0bca3372f7ff794b1
Tori <toriroettger@gmail.com>
2023-12-07 16:35:46 -0600

    make footer responsive for tablet and mobile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p className="footer__text">&copy; 2023 Supersite, Powered by News API</p>
+
       <div className="footer__links">
         <Link to="/" className="footer__link">
           Home
@@ -14,16 +15,19 @@ const Footer = () => {
         <a href="https://tripleten.com/" className="footer__link">
           TripleTen
         </a>
-        <a href="https://github.com/toriroe" className="footer__anchor-icon">
-          <img src={gitHub} className="footer__icon" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/tori-roettger723"
-          className="footer__anchor-icon"
-        >
-          <img src={linkedIn} className="footer__icon footer__icon_last" />
-        </a>
       </div>
+      <ul className="footer__icons">
+        <li className="footer__icon-item">
+          <a href="https://github.com/toriroe">
+            <img src={gitHub} className="footer__icon" />
+          </a>
+        </li>
+        <li className="footer__icon-item">
+          <a href="https://www.linkedin.com/in/tori-roettger723">
+            <img src={linkedIn} className="footer__icon" />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 };
